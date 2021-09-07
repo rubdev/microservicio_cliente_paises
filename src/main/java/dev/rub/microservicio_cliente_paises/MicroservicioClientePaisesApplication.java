c6bdb114c0947f5e2e2b4d1903d4941796c28bd6
Rubén Segura <rsegrom@gmail.com>
2021-09-07 18:57:18 +0200

    Añadir restTemplate

--- src/main/java/dev/rub/microservicio_cliente_paises/MicroservicioClientePaisesApplication.java
+++ src/main/java/dev/rub/microservicio_cliente_paises/MicroservicioClientePaisesApplication.java
@@ -2,12 +2,19 @@ package dev.rub.microservicio_cliente_paises;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MicroservicioClientePaisesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroservicioClientePaisesApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
