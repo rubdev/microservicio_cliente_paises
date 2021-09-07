2acf4b3940617b4b3e89d8a4b84da642413872a7
Rubén Segura <rsegrom@gmail.com>
2021-09-07 18:12:00 +0200

    Creación del proyecto

---
+++ src/main/java/dev/rub/microservicio_cliente_paises/MicroservicioClientePaisesApplication.java
@@ -0,0 +1,13 @@
+package dev.rub.microservicio_cliente_paises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioClientePaisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioClientePaisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/rub/microservicio_cliente_paises/MicroservicioClientePaisesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rub.microservicio_cliente_paises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioClientePaisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
